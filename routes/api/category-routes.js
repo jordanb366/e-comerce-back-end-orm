92181b3e9960a83226e5d1fdee3b5c4968b9661f
Jordan <jordan.b3360@gmail.com>
2022-09-12 15:57:16 -0500

    worked on setting up the RESTful  CRUD operations on api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,13 @@ router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -17,18 +23,51 @@ router.get("/", async (req, res) => {
 router.get("/:id", (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  }).then((categoryId) => {
+    res.json(categoryId);
+  });
 });
 
 router.post("/", (req, res) => {
   // create a new category
+  Category.create(req.body).then((newCategory) => {
+    res.json(newCategory);
+  });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((updatedCategory) => {
+      res.json(updatedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,64 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findByAll({
+    include: [
+      {
+        model: Product,
+        ProductTag,
+      },
+    ],
+  }).then((tagData) => {
+    res.json(tagData);
+  });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [
+      {
+        model: Product,
+        ProductTag,
+      },
+    ],
+  }).then((tagData) => {
+    res.json(tagData);
+  });
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body).then((newTag) => {
+    res.json(newTag);
+  });
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.params.id, {
+    include: [
+      {
+        model: Product,
+        ProductTag,
+      },
+    ],
+  }).then((updatedTag) => {
+    res.json(updatedTag);
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(req.params.id, {}).then((deletedTag) => {
+    res.json(deletedTag);
+  });
 });
 
 module.exports = router;
